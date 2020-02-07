ea303d78f80722dab75465d71a0f435b5a53d765
aleksey <alekseygalkovich@gmail.com>
2020-02-07 15:12:41 +0300

    Добавил меню. Будем пытаться перенести все в классы.

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
 from tkinter import *
 from tkinter.ttk import Combobox
+from tkinter import ttk
 import db_connection
 
 # отрисовываем окно и название окна
-window = Tk()
-window.title("PeripheryX")
-# задаем размеры окна и почему-то сдесь же задаем текст
+root = Tk()
+root.title("PeripheryX")
+root.geometry('1200x800')
+# меню
+menu = Menu(root)
+main_menu = Menu(menu, tearoff=0)
+main_menu.add_command(label='Параметры сервера')
+menu.add_cascade(label='Меню', menu=main_menu)
+root.config(menu=menu)
 
 # текст "Введите данные"
-enter_data = Label(window.geometry('500x400'), text='Введите данные')
+enter_data = Label(text='Введите данные')
 enter_data.grid(column=1, row=0)
 
 # лейбл серийный номер
-sn = Label(window, text='Введите С/Н гарнитуры')
-sn.grid(column=0, row=1)
+sn = Label(root, text='Введите С/Н гарнитуры: ')
+sn.grid(column=0, row=1, sticky=E)
 
 # лейбл рабочее место
-rm = Label(window, text='Введите РМ')
-rm.grid(column=0, row=2)
+rm = Label(root, text='Введите РМ: ')
+rm.grid(column=0, row=2, sticky=E)
 
 # лейбл поломка
-rm = Label(window, text='Неисправность')
-rm.grid(column=0, row=3)
+rm = Label(root, text='Неисправность: ')
+rm.grid(column=0, row=3, sticky=E)
 
 
 # меняем текст при нажатии кнопки
@@ -29,27 +36,26 @@ def click():
     user_data = 'Добавлена гарнитура {}'.format(user_text_sn.get()) + ' c РМ№{}.'.format(user_text_rm.get())
     str(enter_data)
     enter_data.configure(text=user_data)
-    result_user_data()
     insert_data()
 
 
-btn_sn = Button(window, text='Добавить периферию', command=click)
+btn_sn = Button(root, text='Добавить периферию', command=click)
 btn_sn.grid(column=1, row=4)
 
 
 # создаем текстовое поле для кожаного
 
 # поле ввода серийного номера
-user_text_sn = Entry(window, width=22)
+user_text_sn = Entry(root, width=22)
 user_text_sn.grid(column=1, row=1)
 user_text_sn.focus()
 
 # поле ввода рабочего места
-user_text_rm = Entry(window, width=22)
+user_text_rm = Entry(root, width=22)
 user_text_rm.grid(column=1, row=2)
 
 # выпадающий список и поломками
-breakdown_list = Combobox(window)
+breakdown_list = Combobox(root)
 breakdown_list['values'] = ('Укажите из списка',
                             'Микрофон',
                             'Левый наушник',
@@ -58,15 +64,8 @@ breakdown_list['values'] = ('Укажите из списка',
 breakdown_list.current(0)
 breakdown_list.grid(column=1, row=3)
 
-
-def result_user_data():
-    get_user_text_sn = user_text_sn.get()
-    get_user_text_rm = user_text_rm.get()
-    get_breakdown_list = breakdown_list.get()
-    get_user_data_collection = (get_user_text_sn, get_user_text_rm, get_breakdown_list)
-    print(get_user_data_collection)
-
-
+# создаем cursor и connection которые уже есть в db_connection,
+# поэтому просто обращаемся к ним заренее импортировав
 cur = db_connection.cursor
 conn = db_connection.conn
 
@@ -82,4 +81,4 @@ def insert_data():
     conn.close()
 
 
-window.mainloop()
+root.mainloop()
